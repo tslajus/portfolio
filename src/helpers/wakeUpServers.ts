b5ec4942db3acb1901f076182882533e9e13e9ad
Tadas Šlajus <tslajus@gmail.com~>
2023-08-07 08:35:52 +0300

    console.log removed

--- src/helpers/wakeUpServers.ts
+++ src/helpers/wakeUpServers.ts
@@ -8,7 +8,6 @@ const wakeUpServers = async () => {
   try {
     const wakeUpPromises = servers.map((server) => axios.get(server));
     await Promise.all(wakeUpPromises);
-    console.log("Servers are awake!");
   } catch (error) {
     console.error("Error waking up one or more servers:", error);
   }
