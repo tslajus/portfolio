05503174f48f3b68c9b543d874070279aa9c5a43
Tadas Šlajus <tslajus@gmail.com~>
2023-08-07 08:42:53 +0300

    Updated env variable name and wakeUpServers function

--- src/helpers/wakeUpServers.ts
+++ src/helpers/wakeUpServers.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
 
-const servers = [import.meta.env.VITE_HAIKU_APP_HEALTH_URL || ""].filter(
-  Boolean
-);
+const servers = import.meta.env.VITE_PROJECTS_SERVERS?.split(",") || [];
 
 const wakeUpServers = async () => {
   try {
-    const wakeUpPromises = servers.map((server) => axios.get(server));
+    const wakeUpPromises = servers.map((server: string) => axios.get(server));
     await Promise.all(wakeUpPromises);
   } catch (error) {
     console.error("Error waking up one or more servers:", error);
